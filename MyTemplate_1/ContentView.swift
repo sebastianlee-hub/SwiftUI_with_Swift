8f54f72a7a80331ddcd2da92f3544d6305205745
sebastian_lee <merci726@yahoo.com>
2025-10-04 08:00:11 +0900

    Initial Commit

---
+++ MyTemplate_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyTemplate_1
+//
+//  Created by sebastian lee on 10/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyTemplate_1/MyTemplate_1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyTemplate_1App.swift
+//  MyTemplate_1
+//
+//  Created by sebastian lee on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTemplate_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
